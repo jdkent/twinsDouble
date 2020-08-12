ba172b4df11360187b6c1f344770a5208d625194
James Kent <james-kent@uiowa.edu>
2020-08-12 18:04:42 -0500

    add initial app

---
+++ twins_double_app.py
@@ -0,0 +1,34 @@
+
+#!/usr/bin/env python
+
+import datetime
+import statsapi
+
+TWINS_TEAM_ID = 142
+
+yesterday = datetime.date.today() - datetime.timedelta(days=1) 
+
+game_ids = [game['game_id'] for game in statsapi.schedule(date=yesterday, team=TWINS_TEAM_ID)]
+
+# did the twins get any doubles?
+DOUBLE = False
+for game_id in game_ids:
+    boxscore = statsapi.boxscore_data(game_id)
+
+    if boxscore['teamInfo']['away']['id'] == TWINS_TEAM_ID:
+        home_or_away = 'away'
+    elif boxscore['teamInfo']['home']['id'] == TWINS_TEAM_ID:
+        home_or_away = 'home'
+    else:
+        raise ValueError("Cannot Find TWINs for this game")
+
+    doubles = boxscore[home_or_away]['teamStats']['batting']['doubles']
+
+    if doubles > 0:
+        DOUBLE = True
+        break
+
+if DOUBLE:
+    print("Twins scored a double, get some chicken!")
+else:
+    print("No double, no chicken")
